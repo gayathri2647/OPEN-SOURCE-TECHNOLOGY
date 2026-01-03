caefc2eedfbde7ac2abc51994817a6f046e68d94
M.N.Gayathri Prasad <242204@slcs.edu.in>
2026-01-03 16:05:00 +0530

    College Works

--- Exercises/Ex3_Form.php
+++ Exercises/Ex3_Form.php
@@ -8,13 +8,18 @@ echo "
     <h3><ins>FORM</ins></h3>
     <form>
         <input type=\"text\" id=\"name\" placeholder=\"Enter Full Name\" required>
+        <input type=\"email\" id=\"email\" placeholder=\"Enter Email\" required>
+        <input type=\"number\" id=\"age\" placeholder=\"Enter Age\" required>
         <input type=\"submit\" value=\"Submit\" onclick='myfunction()'>
     </form>
 </body>
 <script>
     function myfunction() {
         let x = document.getElementById(\"name\").value;
+        let y = document.getElementById(\"email\").value;
+        let z = document.getElementById(\"age\").value;
         document.write(\"<h3>Welcome  to our College! \" + x + \"</h3>\");
+        alert(\"Form Submitted Successfully!\");
     }
 </script>
 </html>";
